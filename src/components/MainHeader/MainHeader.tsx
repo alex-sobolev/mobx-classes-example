897f16c7fd68cee8900f5feac8c860abcfa2dc4f
Alex Sobolev <alexandr.sobolev@deliveryhero.com>
2021-04-09 16:31:35 +0200

    Eject config

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -2,5 +2,5 @@ import React, { FC, useEffect } from 'react';
 import './MainHeader.css';
 
 export const MainHeader: FC = () => {
-  return <header className="MainHeader">Mobx with classes for base store components</header>;
+  return <header className="MainHeader">An Example of Mobx with Classes for Base Store Components</header>;
 };
